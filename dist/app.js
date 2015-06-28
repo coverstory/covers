5df37b649d026a1d3957ddc15c29fb408ed87b69
Allan Lasser <lasser.allan@gmail.com>
2015-06-27 18:48:59 -0700

    adds a diagonal illustration function

--- dist/app.js
+++ dist/app.js
@@ -21924,14 +21924,19 @@ var React = require('react/addons');
 
 var Illustration = React.createClass({displayName: "Illustration",
 
-  render: function() {
+  diagonal: function() {
     return (
-      React.createElement("g", {id: "cover-illustration", width: "100%", height: "100%"}, 
+      React.createElement("g", {className: "diagonal cover-illustration", width: "100%", height: "100%"}, 
         React.createElement("rect", {x: "0", y: "0", height: "50%", width: "50%", fill: "#0000FF"}), 
         React.createElement("rect", {x: "50%", y: "50%", height: "50%", width: "50%", fill: "#00FF00"}), 
         React.createElement("rect", {x: "25%", y: "25%", height: "50%", width: "50%", fill: "#FFFF00"})
       )
     )
+  },
+
+  render: function() {
+    var illustration = this.diagonal();
+    return illustration;
   }
 
 });
--- src/jsx/illustration.jsx
+++ src/jsx/illustration.jsx
@@ -2,14 +2,19 @@ var React = require('react/addons');
 
 var Illustration = React.createClass({
 
-  render: function() {
+  diagonal: function() {
     return (
-      <g id="cover-illustration" width="100%" height="100%">
+      <g className="diagonal cover-illustration" width="100%" height="100%">
         <rect x="0" y="0" height="50%" width="50%" fill="#0000FF" />
         <rect x="50%" y="50%" height="50%" width="50%" fill="#00FF00" />
         <rect x="25%" y="25%" height="50%" width="50%" fill="#FFFF00" />
       </g>
     )
+  },
+
+  render: function() {
+    var illustration = this.diagonal();
+    return illustration;
   }
 
 });
